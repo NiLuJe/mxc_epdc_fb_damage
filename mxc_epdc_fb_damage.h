7a4fe5048dae5856cdc8f1f20c4014b202881275
NiLuJe <ninuje@gmail.com>
2021-06-09 23:44:39 +0200

    Jot down stuff left TODO

--- mxc_epdc_fb_damage.h
+++ mxc_epdc_fb_damage.h
@@ -3,6 +3,10 @@
 
 #include <linux/mxcfb.h>
 
+// FIXME: Handle the EPDCv2 ioctls & data format.
+//        Either use an union of the two different structs, or flatten everything to a custom full strruct defined here.
+//        (an union probably makes the copy easier in the module, but reads slightly more annoying to handle,
+//         as we'd have to add a new field describing the data format or something).
 struct mxcfb_damage_update
 {
 	int                      overflow_notify;

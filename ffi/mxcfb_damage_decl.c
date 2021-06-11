f580ebf78187523e55ab3a9c84fd2a54dd3bead2
NiLuJe <ninuje@gmail.com>
2021-06-12 01:36:40 +0200

    Add FFI cdecl stuff

---
+++ ffi/mxcfb_damage_decl.c
@@ -0,0 +1,16 @@
+// For use with https://github.com/koreader/ffi-cdecl to re-generate up-to-date FFI declarations ;)
+#include <mxc_epdc_fb_damage.h>
+
+#include "ffi-cdecl.h"
+
+// Keep this in the same order as mxc_epdc_fb_damage.h
+
+// Enums
+cdecl_type(mxcfb_damage_data_format)
+
+// Structs
+cdecl_type(mxcfb_damage_rect)
+cdecl_type(mxcfb_damage_alt_data)
+cdecl_type(mxcfb_damage_data)
+
+cdecl_type(mxcfb_damage_update)
